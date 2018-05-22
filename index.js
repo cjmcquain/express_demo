02c39c51a4389f477fddc7ba768c6a268e0424c6
Charles McQuain <charlesmcquain@gmail.com>
2018-05-21 22:04:47 -0700

    Enabled Morgan logging only in dev environment

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const Joi = require("joi");
 const express = require("express");
 const app = express();
 const logger = require("./logger");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(helmet());
 app.use(logger);
-app.use(morgan("tiny"));
+
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  console.log("Morgan enabled...");
+}
 
 app.use(function(req, res, next) {
   console.log("Authenticating...");
